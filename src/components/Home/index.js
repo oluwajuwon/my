a504e5d122eee7455f593e0cce91f8f5159968b7
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-12-20 13:17:58 +0100

    update colour scheme

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,14 +12,14 @@ const Home = () => {
       <section className='home-intro-section'>
         <section className='home-brief-intro'>
           <h1>
-            Hi, my name is <span className='intro-name'>Oluwajuwon</span> You can call me a tech bro if you want
+          <span role='img'>👋🏿 </span> Hi, my name is <span className='intro-name'>Oluwajuwon</span> You can call me a tech bro if you want
           </h1>
           <p>
             Welcome to my little portfolio where I talk about myself and some of the projects I've participated or currently participating in, tools I've used and some other cool things.
           
             If you're wondering why this portfolio is so plain and too simple, well it's because I got bored on a faithful day and decided to do a little something to kill time.
          
-            I'm guessing you're not here to read my rants <span role='img'>😜</span>, so you can checkout the other pages via the links at the bottom of the page or click on any of the icons below
+            I'm guessing you're not here to read my rants , so you can checkout the other pages via the links at the bottom of the page or click on any of the icons below
           </p>
         </section>
         <section className='home-social-icons'>
