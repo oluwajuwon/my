e286e1f898d6a8659d214a9c1f41b4d149886736
Oluwajuwon Fagbohungbe <oluwajuwonfagbohungbe@Oluwajuwons-MacBook-Pro.local>
2019-12-31 21:38:26 +0100

    update contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './style.css';
+import gitHub from '../../assets/github-logo.png'
+import medium from '../../assets/medium-logo.png';
+import twitter from '../../assets/twitter-logo.png';
+import dev from '../../assets/dev-logo.png';
 
 const Contact = () => {
   return (
@@ -12,13 +16,26 @@ const Contact = () => {
               <span role='img'>☎️</span> HitMeUp
             </h1>
             <p>
-              Want to work with me,
-              Got a project for me,
-              Or you just want to say hi?
+              Want to work with me
+              | | Got a project for me ?
             </p>
           </section>
 
           <section className='main-contact'>
+            <p>📧 <a href="mailto:jfagbohungbe@gmail?Subject=Hi%20Juwonlo" target="_top">Send me an Email </a></p>
+
+            <p>| | you just want to say hi?</p>
+
+            <div className='contact-social-icons'>
+              <a href='https://twitter.com/The_Oluwajuwon' target='_blank' rel='noopener noreferrer'><img src={twitter} alt='Twitter link' /></a>
+            </div>
+
+            <p>| | you just want to see what I've been up to?</p>
+            <div className='contact-social-icons'>
+            <a href='https://github.com/oluwajuwon' target='_blank' rel='noopener noreferrer'><img src={gitHub} alt='gitHub link' /></a>
+            <a href='https://medium.com/@jfagbohungbe' target='_blank' rel='noopener noreferrer'><img src={medium} alt='Medium link' /></a>
+            <a href='https://dev.to/oluwajuwon/' target='_blank' rel='noopener noreferrer'><img src={dev} alt='Dev.to link' /></a>
+            </div>
           </section>
         </section>
       </div>
