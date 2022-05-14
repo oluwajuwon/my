76335add0d70602849034155131877f10e3c0488
oluwajuwon <jfagbohungbe@gmail.com>
2022-05-14 06:26:58 +0100

    update omoge page

--- src/pages/Aduke/Birthday/index.js
+++ src/pages/Aduke/Birthday/index.js
@@ -1,24 +1,71 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import card from './assets/card-cover.png'
 import './style.css';
 
 const Birthday = (props) => {
 
-  const gotoCard = () => {
-    window.open(`https://drive.google.com/file/d/1ROcG4fTR0Nr8LBFmB4odXvzM6WRnneFZ/view?usp=sharing`, '_blank');
-  }
-  
-
   return (
     <div className="omoge-birthday">
       <div className="heading">
-        <h1>Happy Birthday My Baby <span role="img" aria-label="confetti">🎉🥳</span></h1>
-        <p>So this is what I did with the virtual card Idea lol. Click on the image below</p>
+        <h1>Happy Birthday Aduke Mi <span role="img" aria-label="confetti">🎉🥳</span></h1>
+        <p>There are 24 balloons here, one for each year of your birth.</p>
+        <br />
+        <p>My babyyyyyyyyyy, I hope you have the best time today.
+          I pray that all your heart desires come to pass.
+        </p>
+        <p>
+          The other day you asked me how loving you felt like and I never gave you an answer.
+          <br />
+          Well there is only one way to describe it.
+          I feel like spiderman.
+          <br />Loving you makes me feel powerful, it makes me feel like i can do anything i want to do in this life and take on any thing and anyone.
+          It strengthens me in ways that even I don't really understand.
+          This might sound cliche but sometimes i feel like i could fly.
+          There is a warmth that comes from being with you and a feeling of still waters when we're together.
+          I love how warm your hugs feel, i love how you smile when you tease me or when you're giddy about something.
+          I love your smile so much and your smile will always be beautiful to me every night and day.
+          It constantly warms my heart whenever I can make you smile or happy at the slightest, it’s the most peaceful feeling and I give myself a thumbs up every time I’m the reason you’re smiling.
+          I love how thoughtful you are, it's one of the many things I really love about you.
+          I love how caring you are, sometimes i feel like the way you are you'd literally catch a bullet for anyone you love, you literally care so deeply for everyone you love, and I admire that so much about you.
+          I love your eyes so much babeeeee, they feel like the window and door to a world where time does not exist and you can literally get lost in them.
+          I love how much family means to you, I love it so much and it's so admirable babe
+          I cherish every single moment I spend with you.
+          I love everything about you and I will make sure I try to tell you as often and as much as I can.
+          <br />
+          I love how learned you are OMG!!!!!!!!!
+          <br />
+          There is always something new to learn from you, about you, or with you. it's so amazing.
+          <br />
+          I want to love you everyday, with all my strength, with the grace of God and with every fibre of my being and existence.
+          <br />
+          I am tempted to call you my lawyer lol, but we'll keep that for another time lol
+        </p>
+        <h3>I love you so much babe, and I am glad we get to do life together</h3>
       </div>
-      <div className="card-container" onClick={gotoCard}>
-        <img src={card} alt="Card" />
-      </div>
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
+      <div className="balloon" />
     </div>
   )
 }
